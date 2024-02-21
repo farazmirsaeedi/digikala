b0c3cb0cea7a8d5f21023ffe4e4e87cd3e016ad4
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-02-21 15:24:21 +0330

    edit swiper breakpoint

--- main.js
+++ main.js
@@ -2,6 +2,27 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 13,
     spaceBetween: 30,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 7,
+          spaceBetween: 40
+        },
+        1200:{
+            slidesPerView: 13,
+            spaceBetween: 30
+        }
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
