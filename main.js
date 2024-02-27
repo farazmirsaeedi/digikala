b125dd192058ee6ad9b127c3309f428fb5fa3695
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-02-27 16:17:46 +0330

    add secound and third swiper

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ var swiper = new Swiper(".mySwiper", {
     breakpoints: {
         // when window width is >= 320px
         320: {
-          slidesPerView: 3,
+          slidesPerView: 4,
           spaceBetween: 30
         },
         // when window width is >= 480px
         480: {
-          slidesPerView: 3,
+          slidesPerView: 4,
           spaceBetween: 30
         },
         // when window width is >= 640px
@@ -27,4 +27,20 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+var swiperr = new Swiper(".mySwiper-1", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiperrr = new Swiper(".mySwiper-2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+ 
 });
